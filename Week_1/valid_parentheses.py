50e830b13f177c94d1a1047f710d8acafac47a9a
Krrish Lala <k2lala@uwaterloo.ca>
2026-02-23 13:45:45 -0500

    Add isValid function for parentheses validation
    Implement a function to validate parentheses in a string.

--- Week_1/valid_parentheses.py
+++ Week_1/valid_parentheses.py
@@ -1,1 +1,34 @@
+'''
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
 
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.
+
+'''
+
+class Solution:
+  def isValid(self, s : string) -> bool:
+    stack=[]
+    closeToOpen={')' : '(' , ']' : '[' , '}' : '{'}
+    for c in s:
+      if c in closeToOpen:
+        if stack and stack[-1] == closeToOpen[c]:
+          stack.pop()
+        else:
+          return False
+      else:
+        stack.append(c)
+    return True if not stack else False
+
+'''
+
+Time & Space Complexity
+  Time complexity: 
+    O(n)
+  Space complexity: 
+    O(n)
+
+'''
