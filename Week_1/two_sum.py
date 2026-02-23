f7f89e1b783c164e16980fe8139f84aafd6955a9
Krrish Lala <k2lala@uwaterloo.ca>
2026-02-23 10:47:51 -0500

    Document Two Sum solution with complexity analysis
    Added a detailed explanation of the Two Sum solution's time and space complexity.

--- Week_1/two_sum.py
+++ Week_1/two_sum.py
@@ -1,1 +1,24 @@
+'''
+Two Sum
 
+Most Optimised Solution - Use a Hashmap
+
+Complexity Analysis
+
+Time complexity: O(n).
+We traverse the list containing n elements only once. Each lookup in the table costs only O(1) time.
+
+Space complexity: O(n).
+The extra space required depends on the number of items stored in the hash table, which stores at most n elements.
+
+'''
+
+class Solution:
+  def TwoSum(self, nums : List[int], target : int) -> List[int]:
+    hashmap={}
+    for i in range(len(nums)):
+      complement=target-nums[i]
+      if complement in hashmap:
+        return [i,hashmap[complement]]
+      hashmap[nums[i]]=i
+    return []
