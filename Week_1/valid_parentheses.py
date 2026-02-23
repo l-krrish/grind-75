66094a46a9748d320a2393b2eff96d9b52869cb6
Krrish Lala <k2lala@uwaterloo.ca>
2026-02-23 14:04:20 -0500

    Add docstring examples and constraints to valid_parentheses

--- Week_1/valid_parentheses.py
+++ Week_1/valid_parentheses.py
@@ -7,6 +7,30 @@ Open brackets must be closed by the same type of brackets.
 Open brackets must be closed in the correct order.
 Every close bracket has a corresponding open bracket of the same type.
 
+Return true if s is a valid string, and false otherwise.
+
+Example 1:
+
+Input: s = "[]"
+
+Output: true
+Example 2:
+
+Input: s = "([{}])"
+
+Output: true
+Example 3:
+
+Input: s = "[(])"
+
+Output: false
+Explanation: The brackets are not closed in the correct order.
+
+Constraints:
+
+1 <= s.length <= 1000
+
+
 '''
 
 class Solution:
